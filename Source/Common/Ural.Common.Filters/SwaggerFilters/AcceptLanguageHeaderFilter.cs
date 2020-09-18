d0a3259d44b399304fa6ddbc57c268cfc5087a1c
Mehmet Aydin <aydinmehmet@hotmail.com>
2020-09-18 16:43:24 +0300

    A common custom filters project was created for the Ural Boilerplate project.

---
+++ Source/Common/Ural.Common.Filters/SwaggerFilters/AcceptLanguageHeaderFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+
+namespace Ural.Common.Filters.SwaggerFilters
+{
+    /// <summary>
+    /// The AcceptLanguageHeaderFilter Class 
+    /// </summary>
+    /// <seealso cref="Swashbuckle.AspNetCore.SwaggerGen.IOperationFilter" />
+    public class AcceptLanguageHeaderFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Applies the specified operation.
+        /// </summary>
+        /// <param name="operation">The operation.</param>
+        /// <param name="context">The context.</param>
+        /// <exception cref="System.ArgumentNullException">operation</exception>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation == null)
+            {
+                throw new ArgumentNullException(nameof(operation));
+            }
+
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<OpenApiParameter>();
+            }
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name            = "Accept-Language",
+                AllowEmptyValue = true,
+                In              = ParameterLocation.Header,
+                Required        = false,
+                Description     = "en-GB"
+            });
+        }
+    }
+}
